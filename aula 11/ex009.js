1b963ad0f72c922cd234e92a2caf6d53c948db70
HigorCalve <higor.calve@gmail.com>
2022-09-29 11:55:40 -0300

    mod-d (conditions)
    ajustei o ex007.html (input estava dando erro ao usar o Live Server, pois estava sem um <label>)
    
    condições simples usando if / else

---
+++ aula 11/ex009.js
@@ -0,0 +1,9 @@
+// com Node Exec instalado, pressione F8
+
+var país = 'EUA'
+console.log(`Vivendo em ${país} você é `)
+if (país == 'BRA') {
+    console.log('Brasileiro.')
+} else {
+    console.log('Estrangeiro.')
+}
