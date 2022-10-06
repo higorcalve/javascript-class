f4acbb1a20ac6fab4064d7d4b27fefe8f4c5c065
HigorCalve <104517450+higorcalve@users.noreply.github.com>
2022-10-06 15:09:30 -0300

    repeticoes (#15)
    Laços usando:
    WHILE
    DO-WHILE

---
+++ aula13/teste02.js
@@ -0,0 +1,5 @@
+var c = 1 // variável contador
+while (c <= 500){ // pré-testada
+    console.log(`Passo ${c}`)
+    c++// incremento contador
+}
---
+++ aula13/teste03.js
@@ -0,0 +1,5 @@
+var c = 1 // variável contador
+do { // pós-testada
+    console.log(`Passo ${c}`)
+    c++ // incremento contador
+} while (c <= 6) // condição
