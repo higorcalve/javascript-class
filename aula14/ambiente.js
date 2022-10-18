ea01866d6f9774753f36ae6d4c30a4c1dfbcc985
HigorCalve <104517450+higorcalve@users.noreply.github.com>
2022-10-18 11:06:48 -0300

    aula-14 (#16)
    estrutura de repetição com variável de controle:
    
    for (inicio ; teste ; incr)

---
+++ aula14/ambiente.js
@@ -0,0 +1,13 @@
+/*
+var c = 1 // variável contador
+while (c <= 10){ // pré-testada
+    console.log(`Passo ${c}`)
+    c++// incremento contador
+}
+*/
+
+console.log('Vai começar...')
+for (var c=1 ; c<=10 ; c++) {
+    console.log(`Passo ${c}`)
+}
+console.log('Fim do programa.')
