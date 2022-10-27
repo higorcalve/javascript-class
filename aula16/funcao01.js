ab66230767cc0e4c42ba531b38ea8864f83ffd0a
HigorCalve <higor.calve@gmail.com>
2022-10-27 12:43:09 -0300

    aula-16
    Funções:
    - conferindo se é par ou impar
    - soma de 2 números
    - multiplicação do próprio número
    - fatorial (simples e com recursividade)

---
+++ aula16/funcao01.js
@@ -0,0 +1,15 @@
+function parimp(n) {
+    if (n%2 == 0) {
+        return `par`
+    } else {
+        return `impar`
+    }
+}
+
+console.log(parimp(223))
+
+/*
+ou:
+let res = parimp(n)
+console.log(res)
+*/
