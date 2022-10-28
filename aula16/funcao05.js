a96d9ad10a757a61fd37b96924b30d65e1a039f5
HigorCalve <104517450+higorcalve@users.noreply.github.com>
2022-10-28 13:44:18 -0300

    Aula 16b (#22)
    * add-new-js
    
    Adicionei mais 2 arquivos.
    fatorial de forma crescente (funcao04b) e outro arquivo com varias funções juntas (utilizando variáveis para transferir o valor de uma função para outra)
    
    * update funcao05.js
    
    adicionei comentário no arquivo de recursividade.

--- aula16/funcao05.js
+++ aula16/funcao05.js
@@ -4,6 +4,6 @@ function fatorial(n) {
     if (n == 1) {
         return 1
     } else {
-        return n * fatorial(n-1)
+        return n * fatorial(n-1) // n! = n x (n-1)!
     }
 } console.log(fatorial(5))
