621ec903598cdee013356d77bff46aa1d9e5efd6
HigorCalve <104517450+higorcalve@users.noreply.github.com>
2022-12-01 12:25:15 -0300

    create-d003 (#39)
    Criei o Desafio 003.
    Consiste em um botão que exibe um prompt solicitando um número.
    Em seguida ele exibe um alert com seu antecessor e sucessor.

---
+++ desafios/d003/script.js
@@ -0,0 +1,7 @@
+function botao() {
+    let num = Number(prompt('Digite um número qualquer'))
+    let antecessor = num - 1
+    let sucessor = num + 1
+
+    alert(`Antes do número ${num} temos o ${antecessor}.\nDepois do número ${num} temos o ${sucessor}`)
+}
