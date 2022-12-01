63986f90590c8a3e32d8773790817f733b0ba3a1
HigorCalve <104517450+higorcalve@users.noreply.github.com>
2022-12-01 11:39:55 -0300

    correct-ex010 (#38)
    Criei funções separadas para cada botão.

---
+++ exercicios/ex010b/script.js
@@ -0,0 +1,17 @@
+let res = document.querySelector('div#resultado')
+
+function ação1(){
+    res.innerHTML += (`<p>Você clicou no botão Ação 1.</p>`)
+}
+
+function ação2(){
+    res.innerHTML += (`<p>Você clicou no botão Ação 2.</p>`)
+}
+
+function ação3(){
+    res.innerHTML += (`<p>Você clicou no botão Ação 3.</p>`)
+}
+
+function ação4(){
+    res.innerHTML += (`<p>Você clicou no botão Ação 4.</p>`)
+}
