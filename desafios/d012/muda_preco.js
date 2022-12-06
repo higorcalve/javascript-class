fd9350126558f312be628680d789b7e0a1cb9435
HigorCalve <higor.calve@gmail.com>
2022-12-06 17:16:15 -0300

    create-d012
    criei os arquivos para o site que informa a variação de preços.

---
+++ desafios/d012/muda_preco.js
@@ -0,0 +1,7 @@
+function verificar() {
+    let res = document.querySelector('div#resultado')
+    let preçoinicial = Number(prompt(`Qual era o preço anterior do produto?`))
+    let preçoatual = Number(prompt(`Qual o preço atual do produto?`))
+    let diferença = preçoatual - preçoinicial
+    
+}
